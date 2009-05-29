cb4b0410fa92e5f93ad2f54723323b76d40584fa
augusto.azevedo <augusto.azevedo@d7ffd098-2ed8-11de-a28d-194e1977add9>
2009-05-29 18:55:29 +0000

    Modelos coneituais.

---
+++ trunk/docs/SISP_visao_geral.mm
@@ -0,0 +1,144 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1242837087643" ID="Freemind_Link_1896156239" MODIFIED="1242840475533" TEXT="SISP">
+<font BOLD="true" NAME="SansSerif" SIZE="24"/>
+<node CREATED="1242837192685" FOLDED="true" ID="_" MODIFIED="1242845619909" POSITION="right" STYLE="bubble" TEXT="Ger. de Usu&#xe1;rios">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1242837268221" ID="Freemind_Link_1203039738" MODIFIED="1242840521639" TEXT="Cadastro de Usu&#xe1;rios">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1242840363062" ID="Freemind_Link_677286294" MODIFIED="1242840400705" TEXT="Chave P+(matricula do professor) ou E+(matricula do aluno)"/>
+</node>
+</node>
+<node CREATED="1242837230906" ID="Freemind_Link_1767677645" MODIFIED="1242863121192" POSITION="right" STYLE="bubble" TEXT="Ger. de Bens">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1242837306434" ID="Freemind_Link_49893741" MODIFIED="1242840501460" STYLE="bubble" TEXT="Equipamentos">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1242838437549" ID="Freemind_Link_1421754099" MODIFIED="1242863517436" STYLE="bubble" TEXT="Emprestimos">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1242837626026" ID="Freemind_Link_1159318720" MODIFIED="1242839282503" TEXT="Situa&#xe7;&#xe3;o do material para Emprestimo">
+<node CREATED="1242837979324" ID="Freemind_Link_1187788483" MODIFIED="1242838153555" TEXT="Dispon&#xed;vel apenas para uso em sala">
+<node CREATED="1242838550556" ID="Freemind_Link_146528468" MODIFIED="1242838697058" TEXT="Para equipamentos como datashow que somente podem ser usados em sala de aula.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1242837905246" ID="Freemind_Link_176803768" MODIFIED="1242838693938" TEXT="Dispon&#xed;vel para Empr&#xe9;stimo">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1242838493811" ID="Freemind_Link_283774350" MODIFIED="1242839108311" TEXT="Reserva Solicitada">
+<node CREATED="1242838736199" ID="Freemind_Link_1966282523" MODIFIED="1242839114442" TEXT="Para que uma reserva seja confirmada, o empr&#xe9;stimo do material deve ser autorizado pelo professor. O sistema pode colocar um per&#xed;odo de 2 ou 3 dias sem que sejam permitidos pedidos concorrentes de reserva para que seja efetuada a confirma&#xe7;&#xe3;o.">
+<icon BUILTIN="messagebox_warning"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1242838326374" ID="Freemind_Link_866595613" MODIFIED="1242838329073" TEXT="Reservado"/>
+<node CREATED="1242837916642" ID="Freemind_Link_1614896565" MODIFIED="1242838154382" TEXT="Emprestado"/>
+<node CREATED="1242837944528" ID="Freemind_Link_369983070" MODIFIED="1242838154818" TEXT="Baixado / Inutilizado">
+<node CREATED="1242838654313" ID="Freemind_Link_1362681644" MODIFIED="1242838699975" TEXT="Equipamento indispon&#xed;vel para uso por defeito, sem previs&#xe3;o de reparo.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+<node CREATED="1242840292330" ID="Freemind_Link_962692637" MODIFIED="1242840300474" TEXT="Solicita&#xe7;&#xe3;o de Reserva">
+<node CREATED="1242840313484" ID="Freemind_Link_1704397195" MODIFIED="1242840347025" TEXT="Deve consultar pend&#xea;ncias de empr&#xe9;stimos de equipamentos para usu&#xe1;rios"/>
+</node>
+<node CREATED="1242840301542" ID="Freemind_Link_568708689" MODIFIED="1242840307736" TEXT="Autoriza&#xe7;&#xe3;o de Eprestimo"/>
+<node CREATED="1242840572901" ID="Freemind_Link_745419463" MODIFIED="1242863906478" TEXT="Ger. de Ocorrencias de Empr&#xe9;stimo">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="bookmark"/>
+<node CREATED="1242840601707" ID="Freemind_Link_700427752" MODIFIED="1242840608930" TEXT="Inclus&#xe3;o de Ocorr&#xea;ncia"/>
+<node CREATED="1242840609499" ID="Freemind_Link_1912265099" MODIFIED="1242840623703" TEXT="Baixa de ocorr&#xea;ncia"/>
+<node CREATED="1242844327798" ID="Freemind_Link_446731198" MODIFIED="1242844788952" TEXT="Tipos do Ocorr&#xea;ncia">
+<node CREATED="1242844338422" ID="Freemind_Link_1704397125" MODIFIED="1242844347720" TEXT="Impeditivo de novos empr&#xea;stimos">
+<node CREATED="1242844499313" ID="Freemind_Link_1580239695" MODIFIED="1242844511122" TEXT="Devolu&#xe7;&#xe3;o com sinais de mau uso"/>
+<node CREATED="1242844565730" ID="Freemind_Link_1017948572" MODIFIED="1242844584434" TEXT="Atrasos superior ao tolerado para devolu&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1242844348180" ID="Freemind_Link_524227884" MODIFIED="1242844356354" TEXT="Impeditivo de conclus&#xe3;o de curso">
+<node CREATED="1242844515677" ID="Freemind_Link_318974429" MODIFIED="1242844544163" TEXT="Falta de equipamento ou parte na devolu&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1242844422272" ID="Freemind_Link_508266238" MODIFIED="1242844429042" TEXT="Informativo">
+<node CREATED="1242844453870" ID="Freemind_Link_73150270" MODIFIED="1242844475881" TEXT="Baixa de reserva por n&#xe3;o retirada de material"/>
+<node CREATED="1242844639386" ID="Freemind_Link_267841111" MODIFIED="1242844646671" TEXT="Atraso na devolu&#xe7;&#xe3;o do material"/>
+</node>
+</node>
+<node CREATED="1242844652794" ID="Freemind_Link_1936491386" MODIFIED="1242844791432" TEXT="Ocorr&#xea;ncias devem vincular: ALUNO; AUTORIZANTE; DATA; RESERVA;">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1242840899975" ID="Freemind_Link_1657517290" MODIFIED="1242863138273" TEXT="Relat&#xf3;rios Empr&#xe9;stimo">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="flag"/>
+<node CREATED="1242840906239" ID="Freemind_Link_1703166358" MODIFIED="1242840920731" TEXT="Empr&#xe9;stimos de determinado material em um per&#xed;odo">
+<node CREATED="1242844310709" ID="Freemind_Link_912492686" MODIFIED="1242844316746" TEXT="Ocorr&#xea;ncias geradas"/>
+<node CREATED="1242844388802" ID="Freemind_Link_741119211" MODIFIED="1242844398131" TEXT="Tempo m&#xe9;dio de perman&#xea;ncia"/>
+</node>
+<node CREATED="1242840921441" ID="Freemind_Link_175540276" MODIFIED="1242840949927" TEXT="Empr&#xe9;stimos realizados por determinado usu&#xe1;rio em um per&#xed;odo">
+<node CREATED="1242844283557" ID="Freemind_Link_1716988884" MODIFIED="1242844295912" TEXT="Ocorr&#xea;ncias geradas"/>
+</node>
+<node CREATED="1242841022561" ID="Freemind_Link_1350489346" MODIFIED="1242841042374" TEXT="Empr&#xe9;stimos autorizados por determinado usu&#xe1;rio em um per&#xed;odo">
+<node CREATED="1242841049932" ID="Freemind_Link_627836960" MODIFIED="1242841069822" TEXT="Tempo m&#xe9;dio de gap entre solicita&#xe7;&#xe3;o/confirma&#xe7;&#xe3;o"/>
+<node CREATED="1242844194840" ID="Freemind_Link_1885705006" MODIFIED="1242844226180" TEXT="Percentual de autoriza&#xe7;&#xf5;es em rela&#xe7;&#xe3;o ao total do per&#xed;odo"/>
+<node CREATED="1242844232927" ID="Freemind_Link_782560062" MODIFIED="1242844260040" TEXT="Rela&#xe7;&#xe3;o de alunos para os quais foram autorizados empr&#xe9;stimos"/>
+<node CREATED="1242844264041" ID="Freemind_Link_869626685" MODIFIED="1242844276989" TEXT="Ocorr&#xea;ncias geradas"/>
+</node>
+<node CREATED="1242844721052" ID="Freemind_Link_136484201" MODIFIED="1242844746917" TEXT="Reservas canceladas por solicitante em um per&#xed;odo"/>
+<node CREATED="1242844747783" ID="Freemind_Link_868181668" MODIFIED="1242844773492" TEXT="Reservas canceladas por n&#xe3;o retirada do equipamento"/>
+<node CREATED="1242845206457" ID="Freemind_Link_1222340959" MODIFIED="1242845226363" TEXT="Emprestimos com ocorr&#xea;ncia de atraso na devolu&#xe7;&#xe3;o"/>
+<node CREATED="1242845240590" ID="Freemind_Link_1350578743" MODIFIED="1242845282929" TEXT="Empr&#xe9;stimos com ocorrencia de falta de equipamento"/>
+</node>
+</node>
+<node CREATED="1242838929142" ID="Freemind_Link_683246304" MODIFIED="1242863986056" STYLE="bubble" TEXT="Cadastro / Manuten&#xe7;&#xe3;o">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1242838991730" ID="Freemind_Link_1904187791" MODIFIED="1242840153254" TEXT="Inclus&#xe3;o/Manuten&#xe7;&#xe3;o de Marca"/>
+<node CREATED="1242838944376" ID="Freemind_Link_1037911042" MODIFIED="1242839234494" TEXT="Inclus&#xe3;o/Manuten&#xe7;&#xe3;o de material">
+<node CREATED="1242839015700" ID="Freemind_Link_887672353" MODIFIED="1242839201211" TEXT="Vincula&#xe7;&#xe3;o de Acess&#xf3;rios"/>
+<node CREATED="1242839191203" ID="Freemind_Link_1579251836" MODIFIED="1242839199128" TEXT="Vincula&#xe7;&#xe3;o de marca"/>
+<node CREATED="1242845394112" ID="Freemind_Link_282328573" MODIFIED="1242845431092" TEXT="DADOS: data_aquisicao; data_cadastro;"/>
+</node>
+<node CREATED="1242839170533" ID="Freemind_Link_1223624424" MODIFIED="1242839256591" TEXT="Inclu&#xe3;o/Manuten&#xe7;&#xe3;o de acess&#xf3;rio"/>
+<node CREATED="1242839296855" ID="Freemind_Link_1150768669" MODIFIED="1242863786927" TEXT="Situa&#xe7;&#xe3;o do Material">
+<edge STYLE="bezier"/>
+<node CREATED="1242839340653" ID="Freemind_Link_1997920868" MODIFIED="1242863786945" TEXT="Bom para uso">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1242839364436" ID="Freemind_Link_730977865" MODIFIED="1242863786939" TEXT="Baixado / Inutilizado">
+<edge STYLE="bezier"/>
+</node>
+<node CREATED="1242839386096" ID="Freemind_Link_616581880" MODIFIED="1242863786941" TEXT="Baixado / N&#xe3;o devolvido">
+<edge STYLE="bezier"/>
+<node CREATED="1242840192856" ID="Freemind_Link_1763032943" MODIFIED="1242863845402" TEXT="Baixa por n&#xe3;o devolu&#xe7;&#xe3;o devem gerar ocorrencia para o usu&#xe1;rio solicitante do empr&#xe9;stimo do material e para o professor autorizante.">
+<icon BUILTIN="messagebox_warning"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1242837923701" ID="Freemind_Link_496861601" MODIFIED="1242863786942" TEXT="Em manuten&#xe7;&#xe3;o">
+<edge STYLE="bezier"/>
+<node CREATED="1242838625071" ID="Freemind_Link_990250793" MODIFIED="1242863786929" TEXT="Equipamentos ind&#xed;spon&#xed;vle para uso por manuten&#xe7;&#xe3;o.">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1242839478886" ID="Freemind_Link_1428755746" MODIFIED="1242863786943" TEXT="Situa&#xe7;&#xf5;es de baixa devem impactar na disponibilidade de empr&#xe9;stimo.">
+<edge STYLE="bezier"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1242837370940" FOLDED="true" ID="Freemind_Link_1521897598" MODIFIED="1242840506312" STYLE="bubble" TEXT="Salas">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1242837712911" ID="Freemind_Link_1927120534" MODIFIED="1242837720789" TEXT="Cadastro"/>
+<node CREATED="1242837721967" ID="Freemind_Link_1490065059" MODIFIED="1242837726413" TEXT="Manuten&#xe7;&#xe3;o"/>
+<node CREATED="1242837726827" ID="Freemind_Link_1804074077" MODIFIED="1242837728652" TEXT="Reserva"/>
+</node>
+</node>
+<node CREATED="1242837250850" ID="Freemind_Link_820116070" MODIFIED="1242845638454" POSITION="right" STYLE="bubble" TEXT="Ger. de Acessos">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+</map>
